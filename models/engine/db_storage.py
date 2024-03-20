05463435c12071cfedbcaf91e0e03cf9fc03d7c9
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-03-21 01:26:10 +0200

    check

---
+++ models/engine/db_storage.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""db_storge class"""
+from os import getenv
+from models.base_model import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.state import State
+from models.city import City
+
+__objs = {"State": State, "City": City}
+
+class DBStorage:
+    """DBStorage class"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Instatntiates a new model"""
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(getenv('HBNB_MYSQL_USER'),
+                                                 getenv('HBNB_MYSQL_PWD'),
+                                                 getenv('HBNB_MYSQL_HOST'),
+                                                 getenv('HBNB_MYSQL_DB')),
+                                                pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """all method"""
+        _dict = {}
+        if cls is None:
+            for obj in __objs.values():
+                objs = self.__session.query(obj).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    _dict[key] = obj
+        else:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = obj.__class__.__name__ + '.' + obj.id
+                _dict[key] = obj
+        return _dict
+
+    def new(self, obj):
+        """add the object to the current database"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session"""
+        self.__session.delete(obj)
+
+    def reload(self):
+        """relod from db"""
+        from models.state import State
+        from models.base_model import BaseModel, Base
+        from models.city import City
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = Session()
+
+    def close(self):
+        """Close session."""
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='delete',backref="state")
+    
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            """Getter attribute to retrieve associated cities"""
+            from models import storage
+            from models.city import City
+
+            lst = []
+            for city in storage.all(City).values():
+                if city.sate_id == self.id:
+                    lst.append(city)
+            return lst
