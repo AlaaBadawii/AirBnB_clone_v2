ff19785cf6b069a0abc41870f4d0d0f08706ad27
Alaa Badawii <alaa.badawy404@gmail.com>
2026-02-28 13:49:30 +0200

    UPD: state to handle both file and DB storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from datetime import datetime
+from datetime import datetime, time
+from openai import models
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
@@ -10,24 +11,30 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    if models.storage_t == 'db':
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
 
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key in ("created_at", "updated_at"):
-                    setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"
-                    ))
-                else:
+                if key != "__class__" :
                     setattr(self, key, value)
 
+                if kwargs.get("created_at", None) and type(kwargs["created_at"]) is str:
+                    self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.created_at = datetime.utcnow()
+                if kwargs.get("updated_at", None) and type(kwargs["updated_at"]) is str:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.updated_at = datetime.utcnow()
+
+                if kwargs.get("id", None) is None:
+                    self.id = str(uuid.uuid4())                
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
--- models/city.py
+++ models/city.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from openai import models
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
+    if models.storage_t == 'db':
+        __tablename__ = "cities"
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        name = ""
+        state_id = ""
+    
+    def __init__(self, *args, **kwargs):
+        """ Instantiates a new City """
+        super().__init__(*args, **kwargs)
