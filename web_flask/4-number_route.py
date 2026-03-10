ad6b1a42902c15bc2984b94753871ab51cdecda9
Alaa Badawii <alaa.badawy404@gmail.com>
2026-03-10 17:13:54 +0200

    ADD: route /number/<n>

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""scritp that starts Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Home():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return  "C {}".format(text.replace('_', ' '))
+
+@app.route("/python")
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):    
+    return "Python {}".format(text.replace('_', ' '))
+
+@app.route("/number/<int:n>", strict_slashes=True)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
