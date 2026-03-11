dc233387dd0a095822a38126765191fa010993da
Alaa Badawii <alaa.badawy404@gmail.com>
2026-03-11 03:57:21 +0200

    UPD: FileStorage, DBStorage, and State

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,14 @@ from datetime import datetime, timedelta
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
+import models
+
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+    if models.storage_t == 'db':
         id = Column(String(60), nullable=False, primary_key=True)
         created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
         updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
