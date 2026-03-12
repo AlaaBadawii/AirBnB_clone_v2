97c47f051e55d5e27a6eeeedb02eac1b87179d61
Alaa Badawii <alaa.badawy404@gmail.com>
2026-03-12 02:05:05 +0200

    ADD: route /states_list that lists a list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """ Flask app to display a list of states """
-from flask import Flask
+from flask import Flask, render_template
 from models import storage
-
-states = storage.all()
-storage.close()
+from models.state import State
 
 
 app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=True)
+def states_list():
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
+    storage.close()
+    return render_template('7-states_list.html', states=states)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
